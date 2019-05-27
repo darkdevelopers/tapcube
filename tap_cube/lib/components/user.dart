489636900e9a1eac5627055f188e12a5b2f156cd
Marco Schauer <marco.schauer@mdm.de>
2019-05-27 13:20:22 +0200

    add user money

--- tap_cube/lib/components/user.dart
+++ tap_cube/lib/components/user.dart
@@ -8,12 +8,14 @@ class User {
   Sprite userSprite;
   int currentLevel = 1;
   int currentDamage = 1;
+  int currentMoney = 0;
 
-  User(this.gv, double left, double top, {int level, int damage}) {
+  User(this.gv, double left, double top, {int level, int damage, int money}) {
     userSprite = Sprite('user/user.png');
     userRect = Rect.fromLTWH(left, top, gv.tileSize, gv.tileSize);
     currentLevel = level;
     currentDamage = damage;
+    currentMoney = money;
   }
 
   void render(Canvas c) {
