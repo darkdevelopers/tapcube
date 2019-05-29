22a05d082bfd2db583119c443fa3362d836bf6ea
Marco Schauer <blackfire2k7@gmail.com>
2019-05-29 18:31:30 +0200

    change ads to production switch

--- tap_cube/lib/firebase/Ads.dart
+++ tap_cube/lib/firebase/Ads.dart
@@ -1,27 +1,33 @@
 import 'package:firebase_admob/firebase_admob.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io' show Platform;
 
 const String testDevice = 'IphoneSimulator';
 
 class Ads {
   static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
     testDevices: testDevice != null ? <String>[testDevice] : null,
-    keywords: <String>['foo', 'bar'],
-    contentUrl: 'http://foo.com/bar.html',
+    keywords: <String>['idle', 'best games', 'games', 'fun games', 'play free online games', 'play games', 'top games'],
     childDirected: true,
     nonPersonalizedAds: true,
   );
 
-  void init(){
-    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
+  void init() {
+    if (kReleaseMode) {
+      FirebaseAdMob.instance.initialize(appId: getAppId());
+    } else {
+      FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
+    }
   }
 
-  void loadVideoAds(){
-    RewardedVideoAd.instance.listener = (RewardedVideoAdEvent event, {String rewardType, int rewardAmount}){
+  void loadVideoAds() {
+    RewardedVideoAd.instance.listener =
+        (RewardedVideoAdEvent event, {String rewardType, int rewardAmount}) {
       print("RewardedVideoAd event $event");
-      if(event == RewardedVideoAdEvent.loaded){
+      if (event == RewardedVideoAdEvent.loaded) {
         RewardedVideoAd.instance.show();
       }
-      if(event == RewardedVideoAdEvent.failedToLoad){
+      if (event == RewardedVideoAdEvent.failedToLoad) {
         print("FAILED TO LOAD");
       }
       if (event == RewardedVideoAdEvent.rewarded) {
@@ -29,7 +35,18 @@ class Ads {
         print("RewardedVideoAd Amount $rewardAmount");
       }
     };
+    if (kReleaseMode) {
+      RewardedVideoAd.instance
+          .load(adUnitId: getAppId(), targetingInfo: targetingInfo);
+    } else {
+      RewardedVideoAd.instance.load(
+          adUnitId: RewardedVideoAd.testAdUnitId, targetingInfo: targetingInfo);
+    }
+  }
 
-    RewardedVideoAd.instance.load(adUnitId: RewardedVideoAd.testAdUnitId, targetingInfo: targetingInfo);
+  String getAppId() {
+    return Platform.isAndroid
+        ? 'ca-app-pub-8089412545634153/6410165060'
+        : 'ca-app-pub-8089412545634153/6410165060';
   }
 }
