8213fa83a9b5c6294b32de1cd14b4cc17829ff80
Marco Schauer <marco.schauer@mdm.de>
2019-06-11 17:17:38 +0200

    Connect Privacy Terms to Firebase

--- tap_cube/lib/views/datenschutz.dart
+++ tap_cube/lib/views/datenschutz.dart
@@ -1,12 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Datenschutz extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:tap_cube/translation.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+final privacyReference = FirebaseDatabase.instance.reference().child(
+    'general');
+
+class Datenschutz extends StatefulWidget {
+  @override
+  State createState() {
+    return DatenschutzUi();
+  }
+}
+
+class DatenschutzUi extends State<Datenschutz>{
+  StreamSubscription<Event> _onPrivacyChanged;
+  StreamSubscription<Event> _onPrivacyCreated;
+
+  String _privacyText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _onPrivacyCreated =
+        privacyReference.onChildAdded.listen(_onPrivacyAdded);
+    _onPrivacyChanged =
+        privacyReference.onChildChanged.listen(_onPrivacyUpdate);
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    Text content = new Text(
-      'TODO need Datenschutz',
+    Text content = Text(
+      _privacyText.toString(),
       textAlign: TextAlign.center,
       overflow: TextOverflow.ellipsis,
       style: TextStyle(fontWeight: FontWeight.bold),
@@ -16,12 +43,45 @@ class Datenschutz extends StatelessWidget {
       appBar: AppBar(
         title: Text("Datenschutz"),
       ),
-      body: Center(
-        child: new Container(
-          padding: EdgeInsets.all(10.0),
-          child: content
-        )
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: content
       ),
     );
+  }
+
+  void _onPrivacyAdded(Event event) {
+    setState(() {
+      _privacyText = this.fromSnapshot(event.snapshot);
+    });
+  }
+
+  void _onPrivacyUpdate(Event event) {
+    setState(() {
+      _privacyText = this.fromSnapshot(event.snapshot);
+    });
+  }
+
+  String fromSnapshot(DataSnapshot snapshot) {
+    if(snapshot.key == 'privacyterms') {
+      switch (TranslationInformagtions.getLanguage(context)) {
+        case 'de': {
+          return snapshot.value['de'].toString();
+        }
+        break;
+
+        case 'en': {
+          print(snapshot.value['en'].toString());
+          return snapshot.value['en'].toString();
+        }
+        break;
+
+        default: {
+          return snapshot.value['en'].toString();
+        }
+        break;
+      }
+    }
+    return _privacyText;
   }
 }
